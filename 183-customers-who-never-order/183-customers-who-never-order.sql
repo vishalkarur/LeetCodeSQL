7d3bb70af476a5bdd53f40d93002507e2d07478a
Vishal karur <62991525+vishalkarur@users.noreply.github.com>
2022-01-06 15:05:39 +0530

    Delete 183-customers-who-never-order directory

--- 183-customers-who-never-order/183-customers-who-never-order.sql
+++
@@ -1,6 +0,0 @@
-/* Write your PL/SQL query statement below */
-
-
-SELECT NAME  "Customers"
-FROM CUSTOMERS WHERE ID NOT IN (SELECT DISTINCT CUSTOMERID
-                               FROM ORDERS) ;
