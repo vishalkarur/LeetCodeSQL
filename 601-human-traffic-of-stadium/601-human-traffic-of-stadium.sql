4b5ec8933a9e9b0ad78a80f9a665ca3a841449fe
Vishal karur <62991525+vishalkarur@users.noreply.github.com>
2022-01-09 23:32:34 +0530

    Time: 1700 ms (5.71%), Space: 0B (100.00%) - LeetHub

--- 601-human-traffic-of-stadium/601-human-traffic-of-stadium.sql
+++ 601-human-traffic-of-stadium/601-human-traffic-of-stadium.sql
@@ -1,12 +1,3 @@
-with cte1 as (
-    select id, 
-        (id - lag(id,1) over (order by id)) diff_lag1, 
-        (id - lag(id,2) over (order by id)) diff_lag2,
-        (lead(id,1)  over (order by id) - id) diff_lead1,
-        (lead(id,2)  over (order by id) - id) diff_lead2,
-        visit_date, people FROM Stadium where people>=100 order by id
-)
-select id, to_char(visit_date,'YYYY-MM-DD') as visit_date,  people  from cte1 where 
-       (diff_lead1=1 and diff_lead2=2)
-    OR (diff_lead1=1 and diff_lag1=1)
-    OR (diff_lag1=1 and diff_lag2=2)
+SELECT ID "id" , TO_CHAR(VISIT_DATE,'YYYY-MM-DD') "visit_date" , PEOPLE "people" 
+from 
+(SELECT ID , VISIT_DATE, PEOPLE , COUNT(*) OVER (PARTITION BY ID-ROWNUM ORDER BY ID-ROWNUM ) AS CNTER FROM STADIUM WHERE PEOPLE >= 100 ) SS WHERE CNTER >=3 ORDER BY VISIT_DATE
