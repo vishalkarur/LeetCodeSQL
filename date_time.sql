6867b30568c9a55d44b58d62e55ed24d441e1cc6
Vishal karur <62991525+vishalkarur@users.noreply.github.com>
2022-06-03 15:32:44 +0530

    Create date_time.sql

---
+++ date_time.sql
@@ -0,0 +1,24 @@
+select year('2020-05-10')
+
+select hour("2017-06-15 09:34:21")
+
+select month("2017-06-15 09:34:21")
+
+select quarter("2017-06-15 09:34:21")
+
+select date_add("2017-06-15 09:34:21", interval 10 day)
+
+select date_add("2017-06-15 09:34:21", interval 10 month)
+
+select date_add("2017-06-15 09:34:21", interval 10 week)
+
+select date_add("2017-06-15 09:34:21", interval 10 year)
+
+select date_add("2017-06-15 09:34:21", interval 10 quarter)
+
+select date_add("2017-06-15 09:34:21", interval 10 hour)
+
+select date_add("2017-06-15 09:34:21", interval 10 second)
+
+select date_add("2017-06-15 09:34:21", interval 10 minute)
+
